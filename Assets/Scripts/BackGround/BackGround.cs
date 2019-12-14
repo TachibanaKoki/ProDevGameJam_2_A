40901ae6b8c93772097fa2579736c2a431766ae8
tachibanakoki <standchat0820@gmail.com>
2019-12-14 12:36:19 +0900

    背景が変わる機能の追加＋DOSTweenのセットアップ

---
+++ Assets/Scripts/BackGround/BackGround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackGround : MonoBehaviour
+{
+
+	[SerializeField]
+	Color _StartColor = Color.blue;
+	[SerializeField]
+	Color _EndColor = Color.black;
+
+
+	[SerializeField]
+	Image _Image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		_Image.color = Color.Lerp(_StartColor, _EndColor,GameParam._CurrentTime/GameParam._PlayTime);
+	}
+}
---
+++ Assets/Scripts/GameParam.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameParam
+{
+	public static readonly float _PlayTime = 60.0f;
+
+	public static float _CurrentTime = 0;
+
+	public static void Init()
+	{
+		_CurrentTime = 0;
+	}
+}
