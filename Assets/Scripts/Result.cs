c0740718c175b3e73dad73aa02f9423158aca7a3
yuki_ishii <mizuki.izuna@gmail.com>
2019-12-14 12:43:15 +0900

    Result下地

---
+++ Assets/Scripts/Result.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Result : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private SceneObject nextScene;
+
+    private int resultScore = 0;
+    void Start()
+    {
+        //
+        resultScore = Score.m_Score;
+
+
+        //背景の変更
+    }
+
+    // つかわない？
+    void Update()
+    {
+        
+    }
+
+    public void ToNext()
+    {
+        SceneManager.LoadScene( nextScene );
+    }
+}
