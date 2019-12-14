21fe9fd77cbee051aadc057601da2f3b58aa5f78
tachibanakoki <standchat0820@gmail.com>
2019-12-14 12:51:39 +0900

    ゲーム進行管理クラス追加

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public enum State
+	{
+		Start,
+		Play,
+		End
+	}
+
+	public State _State = State.Start; 
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+		DOVirtual.DelayedCall(3.0f,()=> { _State = State.Play; });
+
+		if(_State == State.Play)
+		{
+			GameParam.Update();
+
+			if(GameParam._CurrentTime>= GameParam._PlayTime)
+			{
+				_State = State.End;
+				SceneManager.LoadSceneAsync(2);
+			}
+		}
+    }
+}
