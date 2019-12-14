fe73876022411f571fd2523e741d8f136e40c441
tachibanakoki <standchat0820@gmail.com>
2019-12-14 16:34:58 +0900

    スタート画面

--- Assets/Scripts/Boot/BootManager.cs
+++ Assets/Scripts/Boot/BootManager.cs
@@ -19,7 +19,7 @@ public class BootManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		_StartButton.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -387+(Mathf.Sin(Time.time*2)*20.0f),0);
     }
 
 	private void OnStart()
