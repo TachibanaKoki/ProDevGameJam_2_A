401efc0b4f188b6924128782de3d567ebeff2149
tachibanakoki <standchat0820@gmail.com>
2019-12-14 12:26:09 +0900

    スタチックなスコアクラス追加

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Score
+{
+	public static int m_Score = 0;
+
+	public static void Init()
+	{
+		m_Score = 0;
+	}
+}
