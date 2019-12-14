31a76c44a9aec39bec11192ce1f905c377acc481
tachibanakoki <standchat0820@gmail.com>
2019-12-14 11:42:56 +0900

    ブートシーン作成

---
+++ Assets/Scripts/BootManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BootManager : MonoBehaviour
+{
+	[SerializeField]
+	Button _StartButton;
+
+	AsyncOperation _AsyncOperation;
+    // Start is called before the first frame update
+    void Start()
+    {
+		_StartButton.onClick.AddListener(OnStart);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnStart()
+	{
+		if (_AsyncOperation != null) return;
+		_AsyncOperation = SceneManager.LoadSceneAsync(1);
+	}
+}
